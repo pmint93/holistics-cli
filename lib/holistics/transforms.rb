3144badb8bf08c2f5238e385b8942b89d862e787
Thanh Pham Minh <phamminhthanh69@gmail.com>
2017-10-11 01:15:18 +0700

    Fix uncompleted transform info command

--- lib/holistics/transforms.rb
+++ lib/holistics/transforms.rb
@@ -30,7 +30,43 @@ Examples:
     desc 'info ID', 'Show a data transform by ID'
     def info(id)
       puts "`transform info #{id}` called with options: #{options}" if Holistics.debug?
-      tp(@this.find(id))
+      item = @this.find(id)
+      # General info
+      {
+        id: item['id'].to_s.yellow,
+        title: item['title'],
+        data_source: item['data_source_id'],
+        data_dest_id: item['data_dest_id'],
+        owner_id: item['owner_id'],
+        dest_schema_name: item['dest_schema_name'],
+        dest_table_name: item['dest_table_name'],
+        tenant_id: item['tenant_id'],
+        settings: item['settings'],
+        created_at: item['created_at'] ? DateTime.parse(item['created_at']).to_formatted_s(:short) : nil,
+        updated_at: item['updated_at'] ? DateTime.parse(item['updated_at']).to_formatted_s(:short) : nil
+      }.each { |k, v| puts [k.to_s.upcase, v].join(": ") }
+      puts ""
+      # Query info
+      puts "==> QUERY:"
+      puts item['query'].to_s.magenta
+      puts ""
+      puts "==> COLUMNS:"
+      tp(item['table_config']['columns'], *%w{column_name data_type is_nullable})
+      puts ""
+      {
+        dist_key: item['dist_key'],
+        dist_style: item['dist_style'],
+        sort_keys: item['sort_keys'],
+        sort_style: item['sort_style'],
+        enable_upsert: item['enable_upsert'],
+        upsert_column: item['upsert_column'],
+        increment_column: item['increment_column']
+      }.each { |k, v| puts [k.to_s.upcase, v].join(": ") }
+      puts ""
+      # Schedules
+      puts "==> SCHEDULES:"
+      tp(item['schedules'])
+      puts ""
     end
 
     desc 'execute ID', 'Execute a transform job by ID'
