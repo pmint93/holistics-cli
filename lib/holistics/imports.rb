15d2de6e13d86b40ab0d3966636a9e2e761d054a
Thanh Pham Minh <phamminhthanh69@gmail.com>
2017-10-11 02:44:35 +0700

    Enable [import info] command

--- lib/holistics/imports.rb
+++ lib/holistics/imports.rb
@@ -51,11 +51,49 @@ Examples:
       )
     end
 
-    # desc 'info ID', 'Show a import job by id'
-    # def info(id)
-    #   puts "`imports info #{id}` called with options: #{options}" if Holistics.debug?
-    #   tp(@this.find(id))
-    # end
+    desc 'info ID', 'Show a import job by id'
+    def info(id)
+      puts "`imports info #{id}` called with options: #{options}" if Holistics.debug?
+      item = @this.find(id)
+      # General info
+      {
+        id: item['id'].to_s.yellow,
+        title: item['title'],
+        description: item['description'],
+        source_type: item['source_type'],
+        owner_id: item['owner_id'],
+        dest_ds_id: item['dest_ds_id'],
+        import_mode: item['import_mode'],
+        dest_schema_name: item['dest_schema_name'],
+        dest_table_name: item['dest_table_name'],
+        tenant_id: item['tenant_id'],
+        created_at: item['created_at'] ? DateTime.parse(item['created_at']).to_formatted_s(:short) : nil,
+        updated_at: item['updated_at'] ? DateTime.parse(item['updated_at']).to_formatted_s(:short) : nil
+      }.each { |k, v| puts [k.to_s.upcase, v].join(": ") }
+      puts ""
+      # Query info
+      puts "==> SOURCE CONFIG:"
+      puts item['source_config'].to_yaml.gsub(/^---\n/, '')
+      puts ""
+      puts "==> COLUMNS:"
+      tp(item['table_config']['columns'], *%w{source_expression column_name data_type is_nullable})
+      puts ""
+      {
+        dist_key: item['dist_key'],
+        dist_style: item['dist_style'],
+        sort_keys: item['sort_keys'],
+        sort_style: item['sort_style'],
+        increment_column: item['increment_column']
+      }.each { |k, v| puts [k, v].join(": ") }
+      puts ""
+      # Schedules
+      puts "==> SCHEDULE:"
+      {
+        schedule_id: item['schedule_id'],
+        schedule: item['schedule']
+      }.each { |k, v| puts [k, v].join(": ") }
+      puts ""
+    end
 
     desc 'execute', 'Execute a import job by ID'
     def execute(id)
